3ac476fd5c48725e647a7673f75fdd7eddcc8d3f
nicogno <cogno.nicolo@gmail.com>
2022-12-09 10:42:47 +0100

    Multi file parse

--- survival.py
+++ survival.py
@@ -32,24 +32,29 @@ headers_complete = lines[fileIndex[0]+1].split("\t")
 headers = [e for e in headers_complete if e not in ('Index', 'Single-Junction Chromosomes', 'Multi-Junction Chromosomes', 'Normal Chromosomes', 'Initial DNA Fragmentation', 'Potential DNA loss')]
 headers_indices = [i for i in range(len(headers_complete)) if headers_complete[i] in headers]
 
-for f in range(len(fileIndex)-1):
-    if f==0:
-        files.append(lines[fileIndex[f]+2:fileIndex[f+1]-1]) # Skip header
-    else:
-        files.append(lines[fileIndex[f]+1:fileIndex[f+1]-1])
-files.append(lines[fileIndex[-1]+1:-1])   # Last file
+if len(fileIndex) == 1:
+    files.append(lines[fileIndex[0]+2:-1]) # Skip header in first file
+else:
+    for f in range(len(fileIndex)):
+        if f==0:    # First 
+            files.append(lines[fileIndex[f]+2:fileIndex[f+1]-1]) # Skip header
+        elif f==len(fileIndex)-1: # Last
+            files.append(lines[fileIndex[-1]+1:-1])   # Last file
+        else:   # Middle
+            files.append(lines[fileIndex[f]+1:fileIndex[f+1]-1])
 
 for file in range(len(files)):
     for line in range(len(files[file])):
         files[file][line] = files[file][line].split("\t")
 
-
 for file in files:
-    summaries = np.zeros(len(headers))
+    summaries = np.zeros(len(headers)+1) # Add one for single hit repair
     for line in file:
         if line[1]!="No misrepair!":
             for n in range(len(headers)):
                 summaries[n]+=int(line[headers_indices[n]])
+            summaries[-1] += int(line[1])-int(line[2])-int(line[3]) # Total breaks - Residual - Misrepaired
     for n, field in enumerate(headers):
         print("total "+field+" "+str(summaries[n]))
+    print("Single Hit Repairs "+str(summaries[-1]))
     print("\n")
